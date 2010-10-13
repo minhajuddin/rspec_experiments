ad16dcfc8baea15012ad2587801cca7d8f9675dd
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-10-13 06:04:37 -0700

    finished the evaluate stuff

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,17 +1,47 @@
 class Calc
+
   def add(first, second)
     second + first
   end
 
   def evaluate(expression)
-    tokens = expression.split('+')
+    expression.reduce('+', proc{ |x,y| x + y }, proc{ |expr|
+      expr.reduce('-', proc{ |x,y| x - y }, proc{ |expr| 
+        expr.reduce('*', proc{ |x,y| x * y }, proc{ |expr| 
+          expr.reduce('/', proc{ |x,y| x / y }, proc{ |expr| 
+            expr.reduce('^', proc{ |x,y| 
+              result = 1
+              y.times.each do
+                result = result * x
+              end
+              result
+            }, proc{ |expr| expr.to_i })
+          })
+        })
+      })
+    })
+  end
 
-    tokens.inject(0) do |sum, token|
-      result = token.split('-').collect {|x| x.to_i}.inject do |r, t|
-        r -= t
+end
+
+class String
+  def reduce(delimiter, operation, further)
+    tokens = self.split(delimiter)
+
+    result = 0
+
+    tokens.each_with_index do |token, index|
+
+
+      if index == 0
+        result = further.(token)
+      else
+        result = operation.(result, further.(token))
       end
-      sum += result
+
+
     end
 
+    return result
   end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -6,6 +6,7 @@ describe Calc do
     @calc = Calc.new
   end
 
+
   context "#add" do
 
     it "returns 210 when 202 and 8 are passed" do
@@ -39,12 +40,50 @@ describe Calc do
         @calc.evaluate("22-13").should == 9
       end
 
-      #it "returns 9 for 2+7+28" do
-      #@calc.evaluate("2+7+28").should == 37
-      #end
+      it "returns 9 for 2+7+28" do
+        @calc.evaluate("2+7+28-20").should == 17
+      end
 
     end
 
+    context "#multiplication" do
+
+      it "returns 99 for 11*9" do
+        @calc.evaluate("11*9").should == 99
+      end
+
+      it "returns 11 for 2+7*2-5" do
+        @calc.evaluate("2+7*2-5").should == 11
+      end
+
+    end
+
+    context "#division" do
+
+      it "returns 9 for 99/11" do
+        @calc.evaluate("99/11").should == 9
+      end
+
+      it "returns 11 for 2+7*2-5+6/2" do
+        @calc.evaluate("2+7*2-5+6/2").should == 14
+      end
+
+    end
+
+    context "#power" do
+
+      it "returns 1024 for 2^10" do
+        @calc.evaluate("2^10").should == 1024
+      end
+    end
+
+
   end
 
+end
+
+describe String do
+  it "returns 5 for 2+3" do
+    "2+3".reduce('+', proc{|x, y| x + y }, proc{|expr| expr.to_i}).should == 5
+  end
 end
